ea8fa2c2ec20f5ed54f592db42c9d56cda2998b1
James Freeman-Gray <jfg@macbookair.home>
2026-03-04 21:14:39 +0000

    add staging password protection

---
+++ middleware.js
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(request) {
+  const basicAuth = request.headers.get('authorization')
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, password] = atob(authValue).split(':')
+
+    if (user === 'mutomorro' && password === 'yourpasswordhere') {
+      return NextResponse.next()
+    }
+  }
+
+  return new NextResponse('Protected', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Staging"',
+    },
+  })
+}
+
+export const config = {
+  matcher: '/((?!_next/static|_next/image|favicon.ico).*)',
+}
